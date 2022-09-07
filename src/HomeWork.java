0e446066c22e9a045fba81317f74eb7cc430c87f
STONE17th <17th@inbox.ru>
2022-09-08 00:19:13 +0300

    Обновлен файл-навигации HomeWork

--- src/HomeWork.java
+++ src/HomeWork.java
@@ -9,45 +9,19 @@ public class HomeWork {
             case (1) -> System.out.printf("Задание %d еще не готово", number);
             case (2) -> Task002.main(args);
             case (3) -> Task003.main(args);
-
-//            case (4): {
-//                System.out.printf("Задание %d еще не готово", number);
-//                break;
-//            }
-//            case (5): {
-//                System.out.printf("Задание %d еще не готово", number);
-//                break;
-//            }
-//            case (6): {
-//                System.out.printf("Задание %d еще не готово", number);
-//                break;
-//            }
+            case (4) -> System.out.printf("Задание %d еще не готово", number);
+            case (5) -> System.out.printf("Задание %d еще не готово", number);
+            case (6) -> System.out.printf("Задание %d еще не готово", number);
             case (7) -> Task007.main(args);
             case (8) -> {
                 System.out.println("Частный случай для доски 5*5, универсальное решение в разработке");
                 Task008.main(args);
             }
-
-//            case (9): {
-//                System.out.printf("Задание %d еще не готово", number);
-//                break;
-//            }
-//            case (10): {
-//                System.out.printf("Задание %d еще не готово", number);
-//                break;
-//            }
-//            case (11): {
-//                System.out.printf("Задание %d еще не готово", number);
-//                break;
-//            }
-//            case (12): {
-//                System.out.printf("Задание %d еще не готово", number);
-//                break;
-//            }
-//            case (13): {
-//                System.out.printf("Задание %d еще не готово", number);
-//                break;
-//            }
+            case (9) -> System.out.printf("Задание %d еще не готово", number);
+            case (10) -> System.out.printf("Задание %d еще не готово", number);
+            case (11) -> System.out.printf("Задание %d еще не готово", number);
+            case (12) -> System.out.printf("Задание %d еще не готово", number);
+            case (13) -> System.out.printf("Задание %d еще не готово", number);
             default -> System.out.println("Такого задания нет");
         }
     }
